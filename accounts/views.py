b2c8e9e5a2f1fab5e9ea1af6ad521eb2bb9c616c
sergiumr <s3rgiumr@gmail.com>
2021-10-13 17:54:20 +0300

    added user login/register support

---
+++ accounts/views.py
@@ -0,0 +1,62 @@
+from django.contrib import messages, auth
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+
+def register(request):
+   if request.method != 'POST':
+      return render(request, 'accounts/register.html')
+   
+   first_name = request.POST['first_name'].capitalize()
+   last_name = request.POST['last_name'].capitalize()
+   username = request.POST['username']
+   email = request.POST['email']
+   password = request.POST['password']
+   password2 = request.POST['password2']
+   
+   allUsers = User.objects
+   
+   if password != password2:
+      messages.error(request, 'Passwords do not match!')
+      return redirect('register')
+
+   if allUsers.filter(username = username).exists():
+      messages.error(request, 'Username already taken!')
+      return redirect('register')
+   
+   if allUsers.filter(email = email).exists():
+      messages.error(request, 'Email already taken!')
+      return redirect('register')      
+   
+   user = User.objects.create(first_name = first_name, last_name = last_name, username = username, email = email, password = password)
+   
+   user.save()
+   messages.success(request, 'Successfully registered!')
+   return redirect('login')
+   
+def login(request):
+   if request.method != 'POST':
+      return render(request, 'accounts/login.html')
+   
+   username = request.POST['username']
+   password = request.POST['password']
+   
+   user = auth.authenticate(username = username, password = password)
+   
+   if user is None:
+      messages.error(request, 'Invalid credentials!')
+      return redirect('login')
+   
+   auth.login(request, user)
+   messages.success(request, 'You are now logged in!')
+   return redirect('dashboard')
+
+def logout(request):
+   if request.method == "POST":
+      auth.logout(request)
+      messages.success(request, 'You are now logged out.')
+      return redirect('index')
+
+def dashboard(request):
+   return render(request, 'accounts/dashboard.html')
+
+
--- btre/settings.py
+++ btre/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -142,3 +143,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+#Authentication backends
+AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+    )
